9be6d1bbb4593b80f0ec1cfcb843e5095f258589
shkandret <92648471+shkandret@users.noreply.github.com>
2023-07-27 15:22:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+const debounce = (fn, debounceTime) => {
+  let timer;
+  return function debounceFn(...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, debounceTime);
+  };
+};
+
+let repositories;
+const searchInput = document.querySelector(".search__input");
+const searchList = document.querySelector(".search__list");
+const repositoriesList = document.querySelector(".repositories");
+
+const deferredGetRepos = debounce(async function getRepos() {
+  try {
+    if (searchList.innerHTML.length > 0) {
+      searchList.innerHTML = "";
+    }
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${searchInput.value}&per_page=5`
+    );
+    const responseJson = await response.json();
+    repositories = responseJson.items;
+    repositories.forEach((repository, index) => {
+      let fragment = new DocumentFragment();
+      let searchListItem = document.createElement("li");
+      searchListItem.innerText = `${repository.name}`;
+      searchListItem.classList.add("search__list-item");
+      searchListItem.classList.add(`search__list-item${index}`);
+      fragment.append(searchListItem);
+      searchList.append(fragment);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}, 500);
+
+searchInput.addEventListener("keyup", () => {
+  try {
+    if (searchInput.value[0] !== " " && searchInput.value.length !== 0) {
+      deferredGetRepos();
+    } else {
+      searchList.innerHTML = "";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+searchList.addEventListener("click", (elem) => {
+  try {
+    target = elem.target;
+    let targetIndex = target.className
+      .match(/search__list-item[0-9]$/gim)
+      .join("");
+    targetIndex = targetIndex[targetIndex.length - 1];
+    let currentRepository = repositories[targetIndex];
+    searchInput.value = "";
+    searchList.innerHTML = "";
+    let fragment = new DocumentFragment();
+
+    let repositoriesItem = document.createElement("ul");
+    repositoriesItem.innerHTML = `<li>Name: ${currentRepository.name}<br>Owner: ${currentRepository.owner.login}<br>Stars: ${currentRepository.stargazers_count}</li>`;
+    repositoriesItem.classList.add("repositories__item");
+
+    let repositoriesButton = document.createElement("button");
+    repositoriesButton.classList.add("repositories__item-button");
+    repositoriesButton.addEventListener('click', () => {
+      repositoriesItem.remove();
+    })
+    
+    fragment.append(repositoriesItem);
+    repositoriesItem.append(repositoriesButton);
+    repositoriesList.append(fragment);
+  } catch (error) {
+    console.log(error);
+  }
+});
